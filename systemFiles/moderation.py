e9d8eda938418f76d9c649e7d25cff2a830d1c2f
illia841 <illiaholodniak@gmail.com>
2022-06-13 16:16:12 +0300

    Update moderation.py

--- systemFiles/moderation.py
+++ systemFiles/moderation.py
@@ -45,3 +45,15 @@ class Moder(commands.Cog):
         else:
             await ctx.channel.edit(slowmode_delay=seconds)
             await ctx.send(f"Задержка в этом канале установленно на {seconds} секунд!")
+
+
+    # Мьют участников
+    @commands.command()
+    async def mute(self, ctx, member:discord.Member = None):
+        if member == None:
+            await ctx.send('Отметьте участника для мьюта как аргумент')
+        else:
+            await ctx.guild.create_role(name="MG-muted", permissions=discord.Permissions(permissions=1024))
+            mute_role = discord.utils.get(ctx.guild.roles, name="MG-muted")
+            await member.add_roles(mute_role)
+            await ctx.send(f"{member.mention} был замьючен!")
